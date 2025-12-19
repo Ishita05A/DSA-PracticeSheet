fd4d544dbbd7bdf602c0f7674a95d02f7c938669
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-12-19 18:52:25 +0530

    updated code

---
+++ ArrayI/RunningSum.java
@@ -0,0 +1,37 @@
+package ArrayI;
+
+import java.util.Scanner;
+
+public class RunningSum {
+    static void display(int[] arr){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    // prevsum stores the sum of previous elements and at each iteration we are adding the 
+    // current element to previous sum and storing in new array
+    static int[] runningSum(int[] arr){
+        int n = arr.length;
+        int[] running_sum = new int[n];
+        int prevsum = 0;
+        for(int i = 0;i<running_sum.length;i++){
+            running_sum[i] = prevsum+arr[i];
+            prevsum+=arr[i];
+        }
+        return running_sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of Elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] running_sum = runningSum(arr);
+        display(running_sum);
+        sc.close();
+    }
+    
+}
---
+++ ArrayI/minAndMaxOfArray.java
@@ -0,0 +1,54 @@
+package ArrayI;
+
+import java.util.Scanner;
+
+public class minAndMaxOfArray {
+    static int findMinimum_index(int[] arr){
+        int min = Integer.MAX_VALUE;
+        int min_idx = -1;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]<min){
+                min = arr[i];
+                min_idx = i;
+            }
+        } 
+            
+        return min_idx;
+    }
+    static int findMaximum_index(int[] arr){
+        int max = Integer.MIN_VALUE;
+        int max_idx = -1;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+                max_idx = i;
+            }
+        }
+        return max_idx;
+    }
+    static int minimumDeletion(int[] arr){
+        int min_idx = findMinimum_index(arr);
+        int max_idx = findMaximum_index(arr);
+        int ans = 0;
+        int n = arr.length;
+        if(min_idx<n/2) ans+=min_idx+1;
+        else ans+=n-min_idx;
+        if(max_idx<n/2) ans+=max_idx+1;
+        else ans+=n-max_idx;
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of Elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(minimumDeletion(arr));
+        sc.close();
+        
+    }
+    
+}
