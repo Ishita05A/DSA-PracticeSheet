2f7d9104e08d2595c0311bcea68bb7238dc98abc
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-12-20 00:10:50 +0530

    updated code

---
+++ Array/ArrayI/LeftRotateByK.java
@@ -0,0 +1,41 @@
+package Array.ArrayI;
+
+import java.util.Scanner;
+
+public class LeftRotateByK {
+    static void display(int[] arr){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    static void reverse(int[] arr,int i,int j){
+        while (i<j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    static void leftRotateByk(int[] arr,int k){
+        int n = arr.length;
+        k = k%n;
+        reverse(arr, 0, n-k);
+        reverse(arr, n-k+1, n-1);
+        reverse(arr, 0, n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of Elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        leftRotateByk(arr, 3);
+        display(arr);
+        sc.close();
+    }
+    
+}
