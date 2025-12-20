81bf97822055c642aee0abda0dbb7a73f19f0c8c
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-12-20 12:05:16 +0530

    updated code

--- Array/ArrayIII/unionOf2Array.java
+++ Array/ArrayIII/unionOf2Array.java
@@ -1,35 +1,21 @@
 package Array.ArrayIII;
 import java.util.*;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Scanner;
 
-public class IntersectionOf2Array {
-    static void display(int[] arr){
-        for(int i = 0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        
-    }
-    static int[] intersectionOf2Array(int[] arr1,int[] arr2){
-        List<Integer> ans = new ArrayList<>();
-        HashSet<Integer> mp = new HashSet<>();
+public class unionOf2Array {
+    static ArrayList<Integer> findUnion(int[] arr1,int[] arr2){
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeSet<Integer> mp = new TreeSet<>();
         for(int i = 0;i<arr1.length;i++){
             mp.add(arr1[i]);
         }
         for(int i = 0;i<arr2.length;i++){
-           if(mp.contains(arr2[i])) {
-            ans.add(arr2[i]);
-            mp.remove(arr2[i]);
-           }
+            mp.add(arr2[i]);
         }
-        
-        int[] res = new int[ans.size()];
-        for(int i = 0;i<res.length;i++){
-            res[i] = ans.get(i);
+        for(int num:mp){
+            ans.add(num);
         }
-        return res;
-        
+        return ans;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -47,10 +33,8 @@ public class IntersectionOf2Array {
         for(int i = 0;i<m;i++){
             arr1[i] = sc.nextInt();
         }
-        int[] ans = intersectionOf2Array(arr1, arr);
-        display(ans);
+        System.out.println(findUnion(arr1, arr));
         sc.close();
-
     }
     
 }
