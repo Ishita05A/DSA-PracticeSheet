115fc8f21d3cb2c490803a33aed8ec3809386dcb
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-12-20 12:16:21 +0530

    updated code

---
+++ Array/ArrayIII/MaximumSubArraySum.java
@@ -0,0 +1,29 @@
+package Array.ArrayIII;
+
+import java.util.Scanner;
+
+public class MaximumSubArraySum {
+    static int findMaxSubArray_sum(int[] arr){
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0;i<arr.length;i++){
+            sum+=arr[i];
+            max = Math.max(max, sum);
+            if(sum<0) sum = 0;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of Elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(findMaxSubArray_sum(arr));
+        sc.close();
+    }
+    
+}
