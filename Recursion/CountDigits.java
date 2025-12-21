3131019bca63629f18c7ab1741e92341f98b8843
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-12-22 02:53:39 +0530

    updated code

---
+++ Recursion/CountDigits.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    static int countDigits(int n){
+        if(n<=9 && n>0) return 1;
+        return 1+countDigits(n/10); 
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        System.out.println(countDigits(n));
+        sc.close();
+    }
+    
+}
---
+++ Recursion/PrintIncrease.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PrintIncrease {
+    static void printIncreasing(int n){
+        if(n == 0) return;
+        printIncreasing(n-1);
+        System.out.print(n+" ");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        printIncreasing(n);
+        sc.close();
+    }
+    
+}
